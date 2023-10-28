1780a793df7f173ad560b7d1920a247dec0937e2
linskin <1360293790@qq.com>
2023-10-28 17:07:25 +0800

    homepage更新

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,29 +1,174 @@
-<template>
-  <div >
-    <img src="src/assets/kkun.gif" alt="这是坤哥" />
-    <p>这是坤哥的真爱粉</p>
-    <p>这是坤哥的真爱粉</p>
-    <p>这是坤哥的真爱粉</p>
-    <p>这是坤哥的真爱粉</p>
+<template xmlns="">
+<!--  <div style="position:absolute; top:400px; left:50px; max-width:40%;-->
+<!--  background-color:#ffffff; text-align: justify;-->
+<!--  padding:20px 20px 20px; font-size:80%; z-index: 2;opacity:0.7; animation:caption 1s;-->
+<!--  -webkit-animation:caption 1s;">-->
+<!--  <table>-->
+<!--    <tbody>-->
+<!--    <tr><td><i>new</i> Harbour </td><td align="right"> (Vincenzo Lombardo) </td></tr>-->
+<!--    <tr><td>Roberti Palace </td><td align="right"> (SabinoG.Photo) </td></tr>-->
+<!--    <tr><td>Loreto Church </td><td align="right"> (Vincenzo Lombardo) </td></tr>-->
+<!--    <tr><td>Angioino Castle </td><td align="right"> (photogallery of GAL SudEst Barese) </td></tr>-->
+<!--    </tbody>-->
+<!--  </table>-->
+<!--  </div>-->
+
+<!--  &lt;!&ndash; 第二个代码片段 &ndash;&gt;-->
+<!--  <div style="position:relative; text-align: justify; top:10px;-->
+<!--                background-color:#ffffff;-->
+<!--                padding:100px 150px 100px; font-size: 100%;">-->
+<!--    <table width="90%">-->
+<!--      <tbody>-->
+<!--      <tr>-->
+<!--        <td width="100%">-->
+<!--          The International Conference on Atomic and Molecular Data and Their Applications (ICAMDATA) is a continuing series of international conferences that promotes the use of atomic and molecular (AM) data in various fields of science and technology, providing a forum for interaction of AM data producers and users and for information exchange on AM data needs and availability, and fostering the cross-disciplinary cooperation between the AM data producers and users and the coordination of AM data activities and databases worldwide.-->
+<!--          &lt;!&ndash; 这里省略了其余内容 &ndash;&gt;-->
+<!--        </td>-->
+<!--      </tr>-->
+<!--      </tbody>-->
+<!--    </table>-->
+<!--  </div>-->
+  <div>
+    <body style="min-width:1000px;">
+  <img class="a" src="src/assets/photo.png" alt="图片描述">
+  <div class='slider'>
+    <div class='slide1'></div>
+    <div class='slide2'></div>
+    <div class='slide3'></div>
+    <div class='slide4'></div>
   </div>
+
+  <div style="position:absolute; top:400px; left:50px; max-width:40%;
+background-color:#ffffff; text-align: justify;
+padding:20px 20px 20px; font-size:80%; z-index: 2;opacity:0.7; animation:caption 1s;
+-webkit-animation:caption 1s;">
+    credits<br>
+    <table><tbody>
+    <tr><td><i>new</i> Harbour </td><td align="right"> (Vincenzo Lombardo) </td></tr>
+    <tr><td>Roberti Palace </td><td align="right"> (SabinoG.Photo) </td></tr>
+    <tr><td>Loreto Church </td><td align="right"> (Vincenzo Lombardo) </td></tr>
+    <tr><td>Angioino Castle </td><td align="right"> (photogallery of GAL SudEst Barese) </td></tr>
+    </tbody></table>
+  </div>
+
+
+  <div style="position:relative; text-align: justify; top:10px;
+background-color:#ffffff;
+padding:100px 150px 100px; font-size: 100%;">
+
+    <table width="90%">
+      <tbody>
+      <tr>
+        <td width="100%">
+          The International Conference on Atomic and Molecular Data and Their Applications (ICAMDATA) is a continuing series of international conferences that promotes the use of atomic and molecular (AM) data in various fields of science and technology, providing a forum for interaction of AM data producers and users and for information exchange on AM data needs and availability, and fostering the cross-disciplinary cooperation between the AM data producers and users and the coordination of AM data activities and databases worldwide.
+          <br>
+          <br>
+          The topics covered by the Conference are
+          <br>
+          <ol>
+            <li>
+              <b>Application and needs of atomic and molecular data</b>
+              <ul>
+                <li> Astrophysics and atmospheric physics </li>
+                <li> Inertial and magnetic fusion </li>
+                <li> Low and high temperature laboratory plasma and industrial plasmas </li>
+                <li> Lighting sciences and technology </li>
+                <li> Biomedicine and biophysics </li>
+                <li> Combustion and environmental sciences and technology </li>
+                <li> Surface physics, gaseous electronics, solid state optics and spectroscopy, optoelectronics, etc. </li>
+              </ul>
+            </li>
+            <br>
+            <li>
+              <b>Atomic and molecular data production</b>
+              <ul>
+                <li> Data collection, assessment, exchange and dissemination </li>
+                <li>Standardization of data formats </li>
+                <li> AM databases, activities of data centers</li>
+              </ul>
+            </li>
+            <br>
+            <li>
+              <b>Experimental and theoretical methods for atomic and molecular data generation</b>
+              <ul>
+                <li> Atomic and molecular structure, spectroscopy and radiative processes </li>
+                <li> Electron and photon collisions with atoms and molecules </li>
+                <li> Heavy particle collisions </li>
+                <li> Particle-surface interactions. </li>
+              </ul>
+            </li>
+          </ol>
+          <br><br>
+          The scientific programme will include invited and contributed talks in the regular sessions, a poster session and special sessions devoted to uncertainty quantification and database demonstrations.
+          <br><br>
+          12th ICAMDATA, hosted by the PKP (PlasmaKinetics&Processes) research group, constituted in Comitato Organizzatore di ICAMDATA 2020&star;, in the beautiful scenario of the <a href="https://www.comune.moladibari.ba.it/" target="_blank">Mola di Bari town</a>, is organized in cooperation with the <a href="https://www.iaea.org/" target="_blank">International Atomic Energy Agency (IAEA)</a>, with the patronage of the <a href="https://www.istp.cnr.it/" target="_blank">Institute for Plasma Science and Technology (ISTP)</a>, of the <a href="http://www.en.poliba.it/" target="_blank">Polytechnic of Bari</a> and of the <a href="https://www.comune.moladibari.ba.it/" target="_blank">Municipality of Mola di Bari</a>, and the event is sponsored by <a href="https://www.cfa.harvard.edu/sao" target="_blank">Smithsonian Astrophysical Observatory (SAO)</a> and the  <a href="https://www.mdpi.com/journal/entropy" target="_blank">MDPI Journal <i>Entropy</i></a>.
+          <br><br><br>
+          <hr>
+          &star; Comitato Organizzatore di ICAMDATA 2020 (Via E Orabona 4 – Bari 70126 CF: 93503700721) is a non-profit organization whose only scope is to organize the 12th ICAMDATA
+        </td>
+      </tr>
+      </tbody>
+    </table>
+
+  </div>
+
+
+  <div class='logos'>
+    <hr><br>
+    <img src="." width="70%"><br>
+    contact: secretariat<img src="src/assets/photo.png" width=20px>icamdata2022.it
+    <br><br>
+  </div>
+
+
+  </body></div>
+
 </template>
 
 <script>
 export default {
-  name: "HomePAge"
+  name: "HomePAge",
+  data(){
+    return{
+      conferenceInfo: `
+        原子和分子数据及其应用国际会议（ICAMDATA）是一系列连续的国际会议，旨在促进原子和分子（AM）数据在科学和技术各个领域的使用，为AM数据生产者和用户的互动以及关于AM数据需求和可用性的信息交流提供了一个论坛， 促进增材制造数据生产者和用户之间的跨学科合作，以及全球增材制造数据活动和数据库的协调。
+
+会议涵盖的主题是
+原子和分子数据的应用和需求
+天体物理学和大气物理学
+惯性和磁融合
+低温和高温实验室等离子体和工业等离子体
+照明科学与技术
+生物医学和生物物理学
+燃烧与环境科学与技术
+表面物理、气态电子学、固体光学和光谱学、光电子学等
+
+原子和分子数据制作
+数据收集、评估、交流和传播
+数据格式的标准化
+AM数据库，数据中心的活动
+
+原子和分子数据生成的实验和理论方法
+原子和分子结构、光谱学和辐射过程
+电子和光子与原子和分子的碰撞
+重粒子碰撞
+粒子-表面相互作用。
+
+
+科学计划将包括在常会上受邀和贡献的演讲、海报会议和专门讨论不确定性量化和数据库演示的特别会议。
+
+第12届ICAMDATA由PKP（等离子体动力学与过程）研究小组主办，由ICAMDATA 2020年国际有机组织委员会☆组成，在莫拉迪巴里镇的美丽场景中，与国际原子能机构（原子能机构）合作组织，等离子体科学和技术研究所（ISTP），巴里理工学院和莫拉迪巴里市政府赞助，该活动由史密森尼天体物理天文台（SAO）和MDPI杂志熵赞助。
+
+      `,
+    }
+  }
 }
 </script>
 
 <style scoped>
 /* 你可以在这里添加组件的样式 */
-body{
-  background-color: #282828;/* 你可以使用颜色代码或颜色名称 */
-}
-export default {
-data() {
-  return {
-    conferenceInfo:原子和分子数据及其应用国际会议（ICAMDATA）是一系列国际会议以推动科学和技术的发展。,
-  };
-},
-};
+
 </style>
+<!-- 在你的Vue组件文件中，比如 MyComponent.vue -->
+
+<link href="./class_for_homepage.css" rel="stylesheet" type="text/css">
