c8434f6aef2b96bfab7b2995095f5b4a0e90245c
linskin <110439527+linskin@users.noreply.github.com>
2023-10-28 15:00:19 +0800

    init

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <div>
+        <span>13TH ICAMDATA</span>
+      </div>
+    <div>
+      <span>HOME</span>
+    </div>
+    <div>
+      <span><a href="https://physics.nist.gov/Icamdata/Homepage/charter.html">ICAMDATA Chapter</a></span>
+    </div>
+    <div>
+      <span><a href="https://physics.nist.gov/Icamdata/Homepage/icamdata.html">History</a></span>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name:"Header"
+  }
+
+</script>
--- src/components/HelloWorld.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <div>
-      
-    </div>
-  </div>
-</template>
