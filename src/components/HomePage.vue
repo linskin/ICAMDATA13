5ddd3c77c16e5d0d7e6a7df7cf01349ce043dbff
linskin <1360293790@qq.com>
2023-10-28 16:13:36 +0800

    homepage初始化

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div >
     <img src="src/assets/kkun.gif" alt="这是坤哥" />
     <p>这是坤哥的真爱粉</p>
     <p>这是坤哥的真爱粉</p>
@@ -16,4 +16,14 @@ export default {
 
 <style scoped>
 /* 你可以在这里添加组件的样式 */
+body{
+  background-color: #282828;/* 你可以使用颜色代码或颜色名称 */
+}
+export default {
+data() {
+  return {
+    conferenceInfo:原子和分子数据及其应用国际会议（ICAMDATA）是一系列国际会议以推动科学和技术的发展。,
+  };
+},
+};
 </style>
