61d79cb7098b897535361fc9f36ec47020e1699e
linskin <110439527+linskin@users.noreply.github.com>
2023-10-30 19:08:52 +0800

    zhaowei 进行的更改 静态内容填充

--- src/view/Registration.vue
+++ src/view/Registration.vue
@@ -1,3 +1,4 @@
+
 <script>
   export default {
     name: "Registration"
@@ -5,17 +6,6 @@
 </script>
 
 <template>
-  <div>
-    <el-row>
-      <el-col :span="8">
-
-      </el-col>
-      <el-col :span="16">
-        <img src="" alt="" style="height: 200px;width: 200px;background-color: #222222"/>
-        <p>请在此输入内容</p>
-      </el-col>
-    </el-row>
-  </div>
 </template>
 
 <style scoped>
