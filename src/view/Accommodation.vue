afd851230c109fbab71db980b281b83022feb966
linskin <110439527+linskin@users.noreply.github.com>
2023-11-01 22:54:23 +0800

    龙Tory 进行的更改

--- src/view/Accommodation.vue
+++ src/view/Accommodation.vue
@@ -8,6 +8,35 @@
   </el-carousel>
 </template>
 
+<script>
+const imgList = [
+  {
+    id: '1',
+    url: 'https://puui.qpic.cn/vcover_hz_pic/0/o8mbrpo92gni5uc1614137102933/0'
+  },
+  {
+    id: '2',
+    url: "https://puui.qpic.cn/vcover_hz_pic/0/mzc002004jql6911612779226790/0",
+  },
+  {
+    id: '3',
+    url: "https://puui.qpic.cn/vcover_hz_pic/0/mzc002004660qpk1610075881185/0",
+  },
+  {
+    id: '4',
+    url: "https://puui.qpic.cn/vcover_hz_pic/0/mzc00200x9fxrc91603793682527/0",
+  },
+  {
+    id: '5',
+    url: "https://puui.qpic.cn/vcover_hz_pic/0/mzc00200kf4feup1613962380506/0",
+  },
+  {
+    id: '6',
+    url:  "https://puui.qpic.cn/vcover_hz_pic/0/mzc00200oxq32bf1614750187209/0",
+  }]
+</script>
+
+
 <style scoped>
 .el-carousel__item h3 {
   color: #475669;
